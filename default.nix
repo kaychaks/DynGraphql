7bcbe55f64ad2d8881e6bf2898a4e869707c6adc
Kaushik Chakraborty <>
2019-08-01 09:16:32 +0530

    snap with nix works now

--- default.nix
+++ default.nix
@@ -2,15 +2,20 @@
       compiler ? "default"}:
     let
       inherit (nixpkgs) pkgs;
-      haskellPackages = pkgs.haskell.packages.${compiler};
+
+      haskellPackages = if compiler == "default"
+      then pkgs.haskellPackages
+      else pkgs.haskell.packages.${compiler};
+
       modHaskellPackages = haskellPackages.override (old: {
         overrides = self: super : {
           map-syntax = pkgs.haskell.lib.dontCheck super.map-syntax;
+          http-streams = pkgs.haskell.lib.doJailbreak super.http-streams;
         };
       });
     in
         {
-            DynGraphQL = modHaskellPackages.developPackage {
+            DynGraphql = modHaskellPackages.developPackage {
                 returnShellEnv = false;
                 root = ./.;
             };
--- shell.nix
+++ shell.nix
@@ -1,13 +1,17 @@
 { nixpkgs ? import (import ./nixpkgs.nix) {}, compiler ? "default", withHoogle ? false}:
     let
       inherit (nixpkgs) pkgs;
-      haskellPackages = pkgs.haskell.packages.${compiler};
+      
+      haskellPackages = if compiler == "default"
+      then pkgs.haskellPackages
+      else pkgs.haskell.packages.${compiler};
+      
       project = import ./. { inherit nixpkgs compiler; };
     in
     with haskellPackages;
     shellFor {
       inherit withHoogle;
-      packages = p: [ project.DynGraphQL ];
+      packages = p: [ project.DynGraphql ];
       buildInputs = [
                   ghcid
                   snap-templates
