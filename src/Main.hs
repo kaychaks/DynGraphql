81774536103e56bbec74704d843abf6bd56b596a
Kaushik Chakraborty <>
2019-09-18 12:27:40 +0530

    updates(WIP)

--- src/Main.hs
+++ src/Main.hs
@@ -1,28 +1,92 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell    #-}
+
 module Main where
 
-import Papa
+import           Control.Monad.Reader       (ask)
+import           Papa
+-- import           Prelude                    (undefined)
+import qualified Data.ByteString.Char8      as BS
+import           GHC.Generics               (Generic)
 
-import           Snap.Core
-import           Snap.Snaplet
+import           Snap
 import           Snap.Util.FileServe
-import           Snap.Http.Server
+
+
+import qualified GraphQL                    as GQL
+import qualified GraphQL.API                as GQL
+import qualified GraphQL.Internal.Execution as GQL
+import qualified GraphQL.Internal.Output    as GQL
 
 ----------------------------------------
 
-main :: IO ()
-main = quickHttpServe site
 
-site :: Snap ()
-site =
-    ifTop (writeBS "hello world") <|>
-    route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
-          ] <|>
-    dir "static" (serveDirectory ".")
+-- data Order = Order {
+--   _oid     :: Int,
+--   _odate   :: String,
+--   _ocount  :: Int,
+--   _ostatus :: String
+-- } deriving (Generic, Show)
+-- makeLenses ''Order
 
-echoHandler :: Snap ()
-echoHandler = do
+data App = App {
+  _response :: Snaplet GQL.Response
+}
+makeLenses ''App
+
+-- schema
+type Order = GQL.Object "Order" '[] '[]
+
+
+handleQuery :: Handler App GQL.Response ()
+handleQuery = do
+  initResp <- ask
+  writeBS $ BS.pack $ show initResp
+
+graphQLInit :: SnapletInit b GQL.Response
+graphQLInit = makeSnaplet "graphql-response" "" Nothing $ do
+  return $ GQL.PreExecutionFailure $ GQL.singleError GQL.NoAnonymousOperation
+
+siteSnaplet :: SnapletInit App App
+siteSnaplet = makeSnaplet "App" "" Nothing $ do
+  resp <- nestSnaplet "" response $ graphQLInit
+  addRoutes [
+    ("graphql", with response handleQuery),
+    -- ("", writeBS "hello world"),
+    ("echo/:echoparam", echoHandlerSnaplet),
+    ("foo", writeBS "bar"),
+    ("", serveDirectory "./static/")
+    ]
+  return $ App resp
+
+echoHandlerSnaplet :: Handler b v ()
+echoHandlerSnaplet = do
     param <- getParam "echoparam"
     maybe (writeBS "must specify echo/param in URL")
           writeBS param
+
+
+main :: IO ()
+main = serveSnaplet defaultConfig siteSnaplet
+
+-- site :: Snap ()
+-- site =
+--     ifTop (writeBS "hello world") <|>
+--     route [ ("foo", writeBS "bar")
+--           , ("echo/:echoparam", echoHandler)
+--           ] <|>
+--     dir "static" (serveDirectory ".")
+
+-- echoHandler :: Snap ()
+-- echoHandler = do
+--     param <- getParam "echoparam"
+--     maybe (writeBS "must specify echo/param in URL")
+--           writeBS param
+
+
+-- main :: IO ()
+-- main = quickHttpServe site
